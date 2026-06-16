b67af38c1b309e28d25f1067092a4cf826f69060
Nicholas Kane <6445086+nkane@users.noreply.github.com>
2026-06-16 17:11:48 -0400

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.go
@@ -0,0 +1,10 @@
+func hasDuplicate(nums []int) bool {
+    m := map[int]struct{}{}
+    for _, n := range nums {
+        if _, ok := m[n]; ok {
+            return true
+        }
+        m[n] = struct{}{}
+    }
+    return false
+}
